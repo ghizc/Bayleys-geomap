d4d4e732d0e68eaa5fff894b620e705d4bf9a53e
ghizc <ghiz.chebitou@bayleys-bc.co.nz>
2026-04-01 13:40:16 +1300

    Add files via upload

---
+++ store.js
@@ -0,0 +1,28 @@
+// store.js
+
+export const state = {
+    // Database Data
+    currentUser: null,
+    clientsData: [],
+    premisesData: [],
+    allReportsData: [],
+    contactsData: [],
+    
+    // UI State
+    currentMapStyle: null,
+    currentGalleryImages: [],
+    currentGalleryIndex: 0,
+    currentViewedPremise: null,
+    
+    // Filter State
+    activeAdminTab: 'clients',
+    currentAdminSubFilter: 'all',
+    
+    // Modal State
+    currentEditReportId: null,
+    currentEditJobNo: null,
+    currentEditContactId: null,
+    
+    // Global Map Instance
+    mapInstance: null
+};
