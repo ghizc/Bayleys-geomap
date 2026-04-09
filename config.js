3876dbb43ee0271b3b810bdb60d8a0ce465edb32
ghizc <ghiz.chebitou@bayleys-bc.co.nz>
2026-04-10 08:48:08 +1200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+// config.js
+
+export const SUPABASE_URL = 'https://qppsjxvkoihirzicllvg.supabase.co';
+
+// Make sure this next line exists exactly like this:
+export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwcHNqeHZrb2loaXJ6aWNsbHZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5MTg1OTEsImV4cCI6MjA4NjQ5NDU5MX0.aG7O5llSU-VdovwInmDZabvTtOQNPs2aay5cIU1How0';
+
+export const MAPBOX_TOKEN = 'pk.eyJ1IjoiZ2hpei1jIiwiYSI6ImNtbHpsczQwMTA4bGszZnEyOWk3ejdsdzQifQ.Uyr2i4jlcL04Rf8aaEzv-A';
+
+export const GOOGLE_MAPS_KEY = 'AIzaSyAfsH1DWFwEOhVX3LDCmiZ5GazaTPgwWYQ';
+export const GOOGLE_MAP_ID = 'db7ca6fc44b04eb2402e2ac3';
+
+export const STATUS_OPTIONS = ['New', 'Inspection', 'Report', 'Advice', 'Invoice', 'Cancelled', 'Complete'];
+
+export const ADMIN_UUID = 'd53c9e5c-87db-4620-93e7-e1672c5b53ff';
